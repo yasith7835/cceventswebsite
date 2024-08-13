4c810c9ffd4b336ede02c4b1f77ac1f15efc258c
Thakee Nathees <thakeenathees@gmail.com>
2024-08-13 15:58:04 +0530

    some frontend work

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,17 @@ function Footer() {
   
   return (
     <>
-      <p>Footer</p>
+      <div className="mt-1 center-container">
+        <img src="/src/img/logo-header.png" style={{
+          width: '50%'
+        }} />
+      </div>
+
+      <br/>
+
+      <div className="mb-1 center-container" style={{ fontSize: '.6rem', color: '#9c9c9c' }}>
+        <p> © CCET • <u>TERMS OF USE</u> • <u>PRIVACY POLICY</u></p>
+      </div>
     </>
   )
 }
