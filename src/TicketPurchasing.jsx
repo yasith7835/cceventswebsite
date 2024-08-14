1c23c01abd6ba222d01d2baafc96c7834c2ecd61
yasith7835 <132195470+yasith7835@users.noreply.github.com>
2024-08-14 08:31:51 +0530

    added ticketing polic modal, and adjusted many things

--- src/TicketPurchasing.jsx
+++ src/TicketPurchasing.jsx
@@ -3,20 +3,20 @@ import Footer from './Footer.jsx';
 const API_URL = import.meta.env.VITE_API_KEY;
 import { useDispatch } from 'react-redux';
 import { setCurrentPage } from './userSlice';
-
+import "./css/Modal.css";
 
 function TicketPurchasing() {
   const [ntickets, setNTickets] = useState(0);
   const [ticketType, setTicketType] = useState('');
   const [total, setTotal] = useState(0);
+  const [modal, setModal] = useState(true);// Modal is visible as default
   const dispatch = useDispatch();
 
   const calculateTotalPrice = (ntickets, ticketType) => {
     const pricePerTicket = {
       vip: 100,
-      regular: 50,
+      regular: 2000,
     };
-
     const totalPrice = parseInt(ntickets, 10) * pricePerTicket[ticketType];
     setTotal(totalPrice);
   };
@@ -34,25 +34,43 @@ function TicketPurchasing() {
         body: JSON.stringify({ ntickets }),
       });
 
-      if (response.ok) {
+      if (response.status === 200) {
         const result = await response.json();
         console.log(result);
-        dispatch(setCurrentPage('qrpage'));
-
-        
-      } else {
-        console.error('Error: Payment unsuccessful');
-        alert('Payment Failed');
+        dispatch(setCurrentPage('paymentSuccessful'));
+      } 
+      else if(response.status === 403) {
+        const result = await response.json();
+        console.error('Error:', result.error);
+        alert(result.error);
+      }
+      else if(response.status === 400) {
+        const result = await response.json();
+        console.error('Error:', result.error);
+        alert(result.error);
       }
     } catch (error) {
       console.error('Error: Payment unsuccessful', error);
     }
   };
 
-  
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+      document.body.classList.add('active-modal')
+  } else {
+      document.body.classList.remove('active-modal')
+  }
+
+  const handleBackButton = () => {
+    dispatch(setCurrentPage('landing')); // Navigate back to the landing page
+  };
 
   return (
     <>
+      <button onClick={handleBackButton}>Back</button>
       <h3>Tickets</h3>
       <form onSubmit={handleSubmit}>
         <label htmlFor="seatType">Seat Type:</label>
@@ -70,29 +88,50 @@ function TicketPurchasing() {
           <option value="regular">Regular</option>
         </select>
 
-        <label htmlFor="numTickets">Number of Tickets:</label>
-        <input
-          type="number"
-          id="numTickets"
-          name="numTickets"
-          required
-          min="1"
-          onChange={(e) => {
-            setNTickets(e.target.value);
-            calculateTotalPrice(e.target.value, ticketType);
-          }}
-        />
-
-        <label>Total: Rs.{total}</label>
-
-        <input type="submit" value="Checkout" />
+        {ticketType === "vip" ? (
+          <div>
+            <h4>Contact Event Organizers</h4>
+            <p>Achira Senanayake</p>
+            <p>Phone: +94 77 670 5646</p>
+            <p>Email: something@example.com</p>
+          </div>
+        ) : (
+          <>
+            <label htmlFor="numTickets">Number of Tickets:</label>
+            <input
+              type="number"
+              id="numTickets"
+              name="numTickets"
+              required
+              min="1"
+              onChange={(e) => {
+                setNTickets(e.target.value);
+                calculateTotalPrice(e.target.value, ticketType);
+              }}
+            />
+            <label>Total: Rs.{total}</label>
+            <input type="submit" value="Checkout" />
+          </>
+        )}
       </form>
 
-      <div>
-        <h4>Ticket Policy</h4>
-        <p>All sales are final. No refunds or exchanges.</p>
-      </div>
-
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>Ticketing Policy</h2>
+            <h3>Please Note</h3>
+            <ul>
+              <li>Tickets once bought cannot be refunded.</li>
+              <li>Each person is limited to a total of 10 tickets.</li>
+              <li>The number of tickets can only be changed before the payment has been processed.</li>
+              <li>Tickets cannot be resold at any given time.</li>
+            </ul>
+            <p>For more details, please read the Conditions and Privacy Policy mentioned below.</p>
+            <button className="close-modal" onClick={toggleModal}>CLOSE</button>
+          </div>
+        </div>
+      )}
       <Footer />
     </>
   );
